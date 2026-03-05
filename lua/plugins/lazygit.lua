9cd7ef4a24a1fe82867300b20ab04626b1e65451
MicalFelps <micalfelps@proton.me>
2026-03-05 15:33:34 -0500

    lazygit installed

---
+++ lua/plugins/lazygit.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    "kdheepak/lazygit.nvim",
+    cmd = "LazyGit",
+    config = function()
+      vim.api.nvim_set_keymap("n", "<leader>gg", ":LazyGit<CR>", { noremap = true, silent = true })
+    end,
+  },
+}
